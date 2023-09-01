812cec39876792a24c666d1e94b89f393441ff90
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-09-01 19:44:51 +0200

    add cors

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod lobby;
 mod player;
 
 use axum::{
+    http::Method,
     routing::{delete, get, post},
     Router,
 };
 use lobby::Lobby;
 use std::sync::Arc;
 use std::{net::SocketAddr, sync::Mutex};
+use tower_http::cors::{Any, CorsLayer};
 
 use controller::lobby as LobbyController;
 
@@ -16,12 +18,19 @@ use controller::lobby as LobbyController;
 async fn main() {
     let lobby = Arc::new(Mutex::new(Lobby::new()));
 
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST, Method::DELETE])
+        // allow requests from any origin
+        .allow_origin(Any);
+
     // build our application with a route
     let app = Router::new()
         .route("/lobby", get(LobbyController::get))
         .route("/lobby/players", post(LobbyController::add_player))
         .route("/lobby/players/:id", delete(LobbyController::remove_player))
-        .with_state(lobby);
+        .with_state(lobby)
+        .layer(cors);
 
     // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], 4000));
