35bc70413bac179426e2a2743291f7ca3b7fda3c
ortizdaniel <brisk.salt4034@fastmail.com>
2023-09-05 00:23:49 +0200

    Feat/multiple games (#9)

--- src/controller/game.rs
+++ src/controller/game.rs
@@ -5,46 +5,70 @@ use axum::{
     http::StatusCode,
     Json,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::{game::Game, player::Player};
+use crate::domain::{game_manager::GameManager, player::Player};
+
+#[derive(Deserialize)]
+pub struct AddPlayerRequest {
+    nickname: String,
+}
+
+#[derive(Deserialize)]
+pub struct CreateGameRequest {
+    nickname: String,
+}
+
+#[derive(Serialize)]
+pub struct CreateGameResponse {
+    id: String,
+}
+
+#[derive(Serialize)]
+pub struct AddPlayerResponse {
+    id: Uuid,
+}
+
+pub async fn create_game(
+    State(manager): State<Arc<Mutex<GameManager>>>,
+    Json(request): Json<CreateGameRequest>,
+) -> (StatusCode, Json<CreateGameResponse>) {
+    let host_nickname = request.nickname;
+    let mut manager = manager.lock().unwrap();
+    let game_id = manager.create_new_game(&host_nickname);
+
+    (StatusCode::OK, Json(CreateGameResponse { id: game_id }))
+}
 
 pub async fn get_players(
-    State(game): State<Arc<Mutex<Game>>>,
+    State(manager): State<Arc<Mutex<GameManager>>>,
+    Path(game_id): Path<String>,
 ) -> (StatusCode, Json<Vec<Player>>) {
-    let game = match game.lock() {
-        Ok(game) => game.clone(),
-        Err(_) => panic!("no game"),
-    };
+    let manager = manager.lock().unwrap();
+    let players = manager.get_game(&game_id).unwrap().players();
 
-    (StatusCode::OK, Json(game.players().to_vec()))
+    (StatusCode::OK, Json(players.to_vec()))
 }
 
 pub async fn add_player(
-    State(game): State<Arc<Mutex<Game>>>,
+    State(manager): State<Arc<Mutex<GameManager>>>,
+    Path(game_id): Path<String>,
     Json(request): Json<AddPlayerRequest>,
-) -> (StatusCode, Json<Player>) {
-    let player = Player::new(&request.name);
-    match game.lock() {
-        Ok(mut game) => game.add_player(player.clone()),
-        Err(_) => panic!("can't add"),
-    };
-    (StatusCode::OK, Json(player))
+) -> (StatusCode, Json<AddPlayerResponse>) {
+    let mut manager = manager.lock().unwrap();
+    let player_id = manager.add_player(&game_id, &request.nickname);
+
+    (StatusCode::OK, Json(AddPlayerResponse { id: player_id }))
 }
 
 pub async fn remove_player(
-    State(game): State<Arc<Mutex<Game>>>,
-    Path(id): Path<Uuid>,
+    State(game): State<Arc<Mutex<GameManager>>>,
+    Path((game_id, player_id)): Path<(String, Uuid)>,
 ) -> (StatusCode, Json<Option<Player>>) {
-    let removed = game.lock().unwrap().remove_player(&id);
+    let removed = game.lock().unwrap().remove_player(&game_id, &player_id);
     match removed {
         Some(removed) => (StatusCode::OK, Json(Some(removed))),
         None => (StatusCode::NOT_FOUND, Json(None)),
     }
-}
-
-#[derive(Deserialize)]
-pub struct AddPlayerRequest {
-    name: String,
 }
