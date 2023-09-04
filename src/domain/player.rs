81cfbd4b2a5a3476dfdbc23aa1ab5b344ddaf8d9
ortizdaniel <brisk.salt4034@fastmail.com>
2023-09-05 00:30:18 +0200

    change uuid in players (#10)

--- src/domain/player.rs
+++ src/domain/player.rs
@@ -1,22 +1,14 @@
 use serde::Serialize;
-use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
 pub struct Player {
-    id: Uuid,
     pub nickname: String,
 }
 
 impl Player {
     pub fn new(name: &str) -> Self {
-        let id = Uuid::new_v4();
         Player {
-            id,
             nickname: String::from(name),
         }
-    }
-
-    pub fn id(&self) -> &Uuid {
-        &self.id
     }
 }
