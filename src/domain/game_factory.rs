4e5612ae83c15e087501fb21ff9a5315602909ff
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-10-01 09:21:29 +0200

    Use a random alphanumeric 5 character string for the game id

--- src/domain/game_factory.rs
+++ src/domain/game_factory.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
 
 use tokio::sync::mpsc::{self, Receiver, Sender};
-use uuid::Uuid;
 
 use crate::{actor, actor::game::GameCommand};
 
@@ -19,7 +20,7 @@ impl GameFactory {
     pub fn create_new_game(&mut self) -> String {
         let (tx, rx): (Sender<GameCommand>, Receiver<GameCommand>) = mpsc::channel(128);
         tokio::spawn(actor::game::handler(rx));
-        let id = Uuid::new_v4().to_string();
+        let id = self.create_unique_game_id();
         self.game_channels.insert(id.clone(), tx);
 
         id
@@ -27,5 +28,18 @@ impl GameFactory {
 
     pub fn get_game(&self, game_id: &str) -> Option<&Sender<GameCommand>> {
         self.game_channels.get(game_id)
+    }
+
+    fn create_unique_game_id(&self) -> String {
+        loop {
+            let id: String = rand::thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(5)
+                .map(char::from)
+                .collect();
+            if !self.game_channels.contains_key(&id) {
+                return id
+            }
+        }
     }
 }
