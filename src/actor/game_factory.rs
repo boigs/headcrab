2b24663a87cd7c5573c78cf0bb692549266ce6f4
Daniel <brisk.salt4034@fastmail.com>
2023-10-05 12:59:24 +0200

    chore: remove pub keyword and move impl Display to bottom

--- src/actor/game_factory.rs
+++ src/actor/game_factory.rs
@@ -6,7 +6,7 @@ use crate::actor::game::GameCommand;
 use crate::domain::game_factory::GameFactory;
 
 #[derive(Debug)]
-pub enum GameFactoryCommand {
+enum GameFactoryCommand {
     CreateGame {
         response_channel: OneshotSender<GameFactoryResponse>,
     },
@@ -18,25 +18,10 @@ pub enum GameFactoryCommand {
 
 #[allow(clippy::enum_variant_names)]
 #[derive(Debug)]
-pub enum GameFactoryResponse {
+enum GameFactoryResponse {
     GameCreated { game_id: String },
     GameActor { game_channel: Sender<GameCommand> },
     GameNotFound,
-}
-
-impl Display for GameFactoryResponse {
-    fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            formatter,
-            "{}",
-            match self {
-                GameFactoryResponse::GameCreated { game_id } =>
-                    format!("GameCreated(game_id: {game_id})"),
-                GameFactoryResponse::GameActor { game_channel: _ } => "GameActor".to_string(),
-                GameFactoryResponse::GameNotFound => "GameNotFound".to_string(),
-            }
-        )
-    }
 }
 
 pub struct GameFactoryActor {
@@ -140,5 +125,20 @@ async fn handler(mut rx: Receiver<GameFactoryCommand>) {
                 response_channel.send(response).unwrap();
             }
         }
+    }
+}
+
+impl Display for GameFactoryResponse {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            formatter,
+            "{}",
+            match self {
+                GameFactoryResponse::GameCreated { game_id } =>
+                    format!("GameCreated(game_id: {game_id})"),
+                GameFactoryResponse::GameActor { game_channel: _ } => "GameActor".to_string(),
+                GameFactoryResponse::GameNotFound => "GameNotFound".to_string(),
+            }
+        )
     }
 }
