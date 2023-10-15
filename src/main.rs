5f6ec1950d1882f636d409a71079b4f7af208e52
ortizdaniel <brisk.salt4034@fastmail.com>
2023-10-15 22:48:18 +0200

    feat: use logfmt (#48)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::net::{SocketAddr, TcpListener};
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
-    env_logger::init();
+    std_logger::Config::logfmt().init();
 
     let address = SocketAddr::from(([0, 0, 0, 0], 4000));
     let listener = TcpListener::bind(address).expect("Failed to bind to address");
