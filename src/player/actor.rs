32582f52a2c8a52a24a220faa33570655f6bfd3e
Sergi Valbuena Garrido <24233455+svalbuena@users.noreply.github.com>
2024-05-07 18:59:33 +0200

    More should close ws cases (#127)

--- src/player/actor.rs
+++ src/player/actor.rs
@@ -86,13 +86,14 @@ impl PlayerActor {
     }
 
     fn should_close_websocket(error: Error) -> bool {
-        match error {
-            Error::Domain(DomainError::GameAlreadyInProgress(_)) => true,
-            Error::Domain(_) => false,
-            Error::External(ExternalError::WebsocketClosed(_)) => true,
-            Error::External(_) => false,
-            Error::Internal(_) => true,
-        }
+        matches!(
+            error,
+            Error::Domain(DomainError::GameAlreadyInProgress(_))
+                | Error::Domain(DomainError::GameDoesNotExist(_))
+                | Error::Domain(DomainError::PlayerAlreadyExists(_))
+                | Error::External(ExternalError::WebsocketClosed(_))
+                | Error::Internal(_)
+        )
     }
 
     async fn receive_game_wide_message(
@@ -267,10 +268,10 @@ mod tests {
     #[test]
     fn should_close_websocket_is_false() {
         assert!(!PlayerActor::should_close_websocket(Error::Domain(
-            DomainError::GameDoesNotExist("".to_string())
+            DomainError::NonHostPlayerCannotContinueToNextRound("".to_string())
         )));
         assert!(!PlayerActor::should_close_websocket(Error::Domain(
-            DomainError::PlayerAlreadyExists("".to_string())
+            DomainError::NotEnoughRounds(0, 0)
         )));
         assert!(!PlayerActor::should_close_websocket(Error::Domain(
             DomainError::NotEnoughPlayers(0, 0)
@@ -282,6 +283,12 @@ mod tests {
 
     #[test]
     fn should_close_websocket_is_true() {
+        assert!(PlayerActor::should_close_websocket(Error::Domain(
+            DomainError::GameDoesNotExist("".to_string())
+        )));
+        assert!(PlayerActor::should_close_websocket(Error::Domain(
+            DomainError::PlayerAlreadyExists("".to_string())
+        )));
         assert!(PlayerActor::should_close_websocket(Error::Domain(
             DomainError::GameAlreadyInProgress("".to_string())
         )));
