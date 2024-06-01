5faf5ba4f6b6c38573033b4eb2975d7408d28f27
Daniel <brisk.salt4034@fastmail.com>
2024-06-01 21:36:28 +0200

    chore: sort errors

--- src/error/domain_error.rs
+++ src/error/domain_error.rs
@@ -4,16 +4,22 @@ use crate::game::game_fsm::GameFsmState;
 
 #[derive(Clone, Debug, Error, PartialEq)]
 pub enum DomainError {
+    #[error("Cannot reject matching words when voting item is none")]
+    CannotRejectMatchedWordsWhenVotingItemIsNone,
     #[error("The game is already in progress. GameId: '{0}'.")]
     GameAlreadyInProgress(String),
     #[error("The game does not exist. GameId: '{0}'.")]
     GameDoesNotExist(String),
+    #[error("Cannot reject words in the current state.")]
+    InvalidStateForRejectingMatchedWords,
     #[error("Invalid state for submitting Words. ActualState: '{0:?}', ExpectedState: '{1:?}'.")]
     InvalidStateForWordsSubmission(GameFsmState, GameFsmState),
     #[error(
         "Invalid state for submitting a Voting Word. ActualState: '{0:?}', ExpectedState: '{1:?}'."
     )]
     InvalidStateForVotingWordSubmission(GameFsmState, GameFsmState),
+    #[error("Non host cannot reject matched words")]
+    NonHostCannotRejectMatchedWords,
     #[error("Not enough players to start the game. ActualPlayers: '{0}', MinimumPlayers: '{1}'.")]
     NotEnoughPlayers(usize, usize),
     #[error("Not enough rounds to start the game. ActualRounds: '{0}', MinimumRounds: '{1}'.")]
@@ -38,6 +44,10 @@ pub enum DomainError {
     PlayerCannotSubmitNonExistingOrUsedVotingWord(String),
     #[error("A player cannot submit a Voting Word when the current Voting Item is none. Nickname: '{0}'.")]
     PlayerCannotSubmitVotingWordWhenVotingItemIsNone(String),
+    #[error("The rejected player does not exist.")]
+    RejectedMatchedPlayerDoesNotExist,
+    #[error("The rejected matched word does not exist.")]
+    RejectedMatchedWordDoesNotExist,
     #[error(
         "A player cannot submit Words with repeated words. Nickname: '{nickname}', RepeatedWords: '{}'.", .repeated_words.join(",")
     )]
@@ -45,18 +55,8 @@ pub enum DomainError {
         nickname: String,
         repeated_words: Vec<String>,
     },
-    #[error("The player of the current Voting Item cannot submit a Voting Word. Nickname: '{0}'.")]
-    VotingItemPlayerCannotSubmitVotingWord(String),
-    #[error("Cannot reject words in the current state.")]
-    InvalidStateForRejectingMatchedWords,
-    #[error("The rejected matched word does not exist.")]
-    RejectedMatchedWordDoesNotExist,
-    #[error("The rejected player does not exist.")]
-    RejectedMatchedPlayerDoesNotExist,
-    #[error("Non host cannot reject matched words")]
-    NonHostCannotRejectMatchedWords,
-    #[error("Cannot reject matching words when voting item is none")]
-    CannotRejectMatchedWordsWhenVotingItemIsNone,
     #[error("Cannot reject a word that was not previously picked by the player during matching")]
     RejectedMatchedWordWasNotPickedByPlayer,
+    #[error("The player of the current Voting Item cannot submit a Voting Word. Nickname: '{0}'.")]
+    VotingItemPlayerCannotSubmitVotingWord(String),
 }
